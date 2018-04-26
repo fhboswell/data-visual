33c491ba9959ed438d550216efc1a15ab8786fdf
fhboswell <franklin.henry.boswell@gmail.com>
2018-04-25 21:33:15 -0700

    UI fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-  	
-  	puts"here"
-	if cookies[:Survey2Completed] == "true"
-  		redirect_to done_index_url
-  	else
+  
   		render layout: false
-  	end
   	
-  	puts"after"
 
   end
 end
